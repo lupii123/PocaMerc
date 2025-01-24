b2807cd650983db745e4cb65ed6825b579da1241
hevyza1234 <radianticahevyza@gmail.com>
2025-01-24 19:16:10 +0700

    create fashion page

---
+++ app/detail/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Detail = () => {
+    return (
+        <div>Detail</div>
+    )
+}
+
+export default Detail
--- app/fashion/page.jsx
+++ app/fashion/page.jsx
@@ -1,8 +1,172 @@
-import React from 'react'
+'use client';
+import { Card, CardContent, CardTitle, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import Link from "next/link";
+import { useState } from "react";
+
+const fashions = [
+    {
+        artist: "nct 127",
+        title: "NCT 127 - BLACK STARFISH DOLL KEYRING ‘NEO CITY : SEOUL – THE MOMENTUM’",
+        price: "450.000",
+        image: "/fashion/nct-blk-star.webp"
+    },
+    {
+        artist: "nct 127",
+        title: "NCT 127 - BLACK STARFISH DOLL KEYRING ‘NEO CITY : SEOUL – THE MOMENTUM’",
+        price: "450.000",
+        image: "/fashion/nct-blk-star.webp"
+    },
+    {
+        artist: "nct 127",
+        title: "NCT 127 - BLACK STARFISH DOLL KEYRING ‘NEO CITY : SEOUL – THE MOMENTUM’",
+        price: "450.000",
+        image: "/fashion/nct-blk-star.webp"
+    },
+    {
+        artist: "ENHYPEN",
+        title: "ENHYPEN - 2022 GGU GGU PACKAGE",
+        price: "650.000",
+        image: "/fashion/enhypen-pack.webp"
+    },
+    {
+        artist: "ENHYPEN",
+        title: "ENHYPEN - 2022 GGU GGU PACKAGE",
+        price: "650.000",
+        image: "/fashion/enhypen-pack.webp"
+    },
+    {
+        artist: "ENHYPEN",
+        title: "ENHYPEN - 2022 GGU GGU PACKAGE",
+        price: "650.000",
+        image: "/fashion/enhypen-pack.webp"
+    },
+    {
+        artist: "DAY6",
+        title: "DAY6 - OFFICIAL LIGHT BAND VER 3",
+        price: "1.000.000",
+        image: "/fashion/day6-lightband.webp"
+    },
+    {
+        artist: "DAY6",
+        title: "DAY6 - OFFICIAL LIGHT BAND VER 3",
+        price: "1.000.000",
+        image: "/fashion/day6-lightband.webp"
+    },
+    {
+        artist: "TWICE",
+        title: "TWICE - LIGHT BAND KIT",
+        price: "500.000",
+        image: "/fashion/twice-lightband.webp"
+    },
+    {
+        artist: "TWICE",
+        title: "TWICE - LIGHT BAND KIT",
+        price: "500.000",
+        image: "/fashion/twice-lightband.webp"
+    },
+    {
+        artist: "TWICE",
+        title: "TWICE - LIGHT BAND KIT",
+        price: "500.000",
+        image: "/fashion/twice-lightband.webp"
+    },
+]
+
+
 
 const Fashion = () => {
+
+    const postPerPage = 8;
+    const [data, setData] = useState(fashions);
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(postPerPage);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    let pages = [];
+    for (let i = 1; i <= Math.ceil(data.length / postPerPage); i++) {
+        pages.push(i);
+    }
+
     return (
-        <div>Fashion</div>
+        <div className='w-full h-full'>
+            <div className='flex flex-col'>
+                <div className='w-full h-[280px] bg-accent flex flex-col justify-center items-center'>
+                    <h1 className='uppercase text-secondary font-bold'>Fashion</h1>
+                    <h3 className='uppercase text-secondary font-bold'>Collection</h3>
+                </div>
+
+                <div className='container mx-auto my-20'>
+                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+                        {data.slice(startIndex, endIndex).map((item, index) => {
+                            return (
+                                <Link href={"/"} key={index}>
+                                    <Card className="border-0 shadow-lg">
+                                        <CardHeader className="p-4 pb-0">
+                                            <CardTitle className="text-3xl md:text-2xl xl:text-xl font-bold hover:text-secondary transition-all truncate">{item.title}</CardTitle>
+                                            <CardDescription className="uppercase">{item.artist}</CardDescription>
+                                        </CardHeader>
+                                        <CardContent className="p-0 overflow-hidden">
+                                            <img alt="" src={item.image} className="object-cover hover:scale-125 transition-all" />
+                                        </CardContent>
+                                        <CardFooter className="flex flex-col items-start justify-center gap-2 p-4 pt-0">
+                                            <span>Rp.{item.price}</span>
+                                        </CardFooter>
+                                    </Card>
+                                </Link>
+                            );
+                        })}
+                    </div>
+
+                    <Pagination className={"mt-10"}>
+                        <PaginationContent>
+                            <PaginationItem className="cursor-pointer">
+                                <PaginationPrevious
+                                    className={startIndex === 0 ? "pointer-events-none opacity-50" : undefined}
+                                    onClick={() => {
+                                        setStartIndex(startIndex - postPerPage);
+                                        setEndIndex(endIndex - postPerPage);
+                                        setCurrentPage(currentPage - 1);
+                                        window.scrollTo({ top: 100, behavior: "smooth" });
+                                    }}
+                                />
+                            </PaginationItem>
+
+                            {
+                                pages.map((page, index) => {
+                                    return (
+                                        <PaginationItem key={index} className={currentPage === page ? "bg-neutral-100 rounded-md pointer-events-none" : "cursor-pointer"}>
+                                            <PaginationLink
+                                                onClick={() => {
+                                                    setCurrentPage(page);
+                                                    setStartIndex((page - 1) * postPerPage);
+                                                    setEndIndex(page * postPerPage);
+                                                    window.scrollTo({ top: 100, behavior: "smooth" });
+                                                }}
+                                            >
+                                                {page}
+                                            </PaginationLink>
+                                        </PaginationItem>
+                                    )
+                                })
+                            }
+
+                            <PaginationItem className="cursor-pointer">
+                                <PaginationNext
+                                    className={endIndex >= data.length ? "pointer-events-none opacity-50" : undefined}
+                                    onClick={() => {
+                                        setStartIndex(startIndex + postPerPage);
+                                        setEndIndex(endIndex + postPerPage);
+                                        setCurrentPage(currentPage + 1);
+                                        window.scrollTo({ top: 100, behavior: "smooth" });
+                                    }}
+                                />
+                            </PaginationItem>
+                        </PaginationContent>
+                    </Pagination>
+                </div>
+            </div >
+        </div >
     )
 }
 
